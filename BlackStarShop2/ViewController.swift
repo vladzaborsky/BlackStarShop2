e7255383faf8c8eced2ad074c8855951bec64596
Vlad Zaborsky <vlad.zaborsky1@gmail.com>
2021-11-07 22:50:44 +0300

    commit test

--- BlackStarShop2/ViewController.swift
+++ BlackStarShop2/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    let value: String = "Value of some value"
 
 }
 
